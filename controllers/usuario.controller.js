322311ad5bc632eacab1467ffd55d42ebd9a7558
samfelip <samil.arias@campusucc.edu.co>
2022-10-04 17:39:13 -0500

    rutas y funciones del crud, actualizadas

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -6,16 +6,23 @@ const getUser = (req, res) => {
     res.status(200).json({"users":users});
 };
 
-/* const postUser = (req, res) => {
-    
+ const postUser = (req, res) => {
+    const user = new Usuario();
+    const users = new Usuario();
+    res.status(200).json({"users":users});
+
 };
 
 const putUser = (req, res) => {
-    
+    const user = new Usuario();
+    const users = new Usuario();
+    res.status(200).json({"users":users});
 }; 
 
 const deleteUser = (req, res) => {
-    const user = new Usuario("",email,"");
-}; */
-
-module.exports = {getUser,postUser,putUser};
+    const user = new Usuario("",email,"");  
+    if (err) throw err;
+    console.log('userDel was deleted!');
+}; 
+//se agregaron las function de get, post, put y delete
+module.exports = {getUser,postUser,putUser, deleteUser};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const { getUser } = require("../controllers/usuario.controller");
+const { postUser } = require("../controllers/usuario.controller");
+const { putUser } = require("../controllers/usuario.controller");
+const { deleteUser } = require("../controllers/usuario.controller");
 
 const userRoutes = Router();
 
 userRoutes.get('/user', getUser);
-
-/* userRoutes.post('/url', postUser);
-userRoutes.put('/url', putUser);
-userRoutes.delete('/url', deleteUser); */
-
+userRoutes.post('/user', postUser);
+userRoutes.put('/user', putUser);
+userRoutes.delete('/user', deleteUser); 
+//rutas de post, put y delete agregados
 
 /* NOTA: url es lo que va despues de localhost:3000 */
 module.exports = userRoutes;
